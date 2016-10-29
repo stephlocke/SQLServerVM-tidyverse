c6fded0a1772c0354d8d0f8303070736397340fa
Steph Locke <steph.locke@censornet.com>
2016-10-29 19:49:29 +0100

    Simplified install

--- installRpackages.ps1
+++ installRpackages.ps1
@@ -1,5 +1,2 @@
-$file = "installRpackages.R"
-'install.packages("tidyverse", repos = "https://cran.rstudio.com")' | Write-Output -OutVariable $file
-$app = 'C:\Program Files\Microsoft SQL Server\MSSQL13.MSSQLSERVER\R_SERVICES\bin\Rscript.exe' 
-#$app = 'C:\Program Files\R\R-3.3.1\bin\Rscript.exe'
-& $app $file
+cd "C:\Program Files\Microsoft SQL Server\MSSQL13.MSSQLSERVER\R_SERVICES\bin"
+.\Rscript.exe  -e "install.packages('tidyverse', repos = 'https://cran.rstudio.com')"
