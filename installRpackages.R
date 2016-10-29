57fcbeb69f6919d9946325fee98c40984ef2de72
Steph Locke <steph.locke@censornet.com>
2016-10-29 15:57:27 +0100

    First, and inievitable broken commit

---
+++ installRpackages.R
@@ -0,0 +1,1 @@
+install.packages("tidyverse", repos = "https://cran.rstudio.com")
---
+++ installRpackages.ps1
@@ -0,0 +1,4 @@
+$app = 'C:\Program Files\Microsoft SQL Server\MSSQL13.MSSQLSERVER\R_SERVICES\bin\Rscript.exe' 
+# $app = 'C:\Program Files\R\R-3.3.1\bin\Rscript.exe'
+$file = "installRpackages.R"
+& $app $file
