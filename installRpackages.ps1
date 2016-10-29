7bc65d99c32e015efdd7f1c00c1d796ba6c8fd3f
Steph Locke <steph.locke@censornet.com>
2016-10-29 16:23:10 +0100

    Made ps1 standalone

--- installRpackages.ps1
+++ installRpackages.ps1
@@ -1,4 +1,5 @@
-$app = 'C:\Program Files\Microsoft SQL Server\MSSQL13.MSSQLSERVER\R_SERVICES\bin\Rscript.exe' 
-# $app = 'C:\Program Files\R\R-3.3.1\bin\Rscript.exe'
 $file = "installRpackages.R"
+'install.packages("tidyverse", repos = "https://cran.rstudio.com")' | Write-Output -OutVariable $file
+$app = 'C:\Program Files\Microsoft SQL Server\MSSQL13.MSSQLSERVER\R_SERVICES\bin\Rscript.exe' 
+#$app = 'C:\Program Files\R\R-3.3.1\bin\Rscript.exe'
 & $app $file
